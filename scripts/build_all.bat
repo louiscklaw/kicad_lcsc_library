72febcc52361bc4e5c554c4c9847c683b7638ba7
louiscklaw <logickee@gmail.com>
2022-02-11 15:56:50 +0800

    update,

---
+++ scripts/build_all.bat
@@ -0,0 +1,12 @@
+start scripts\build_xaa.bat
+start scripts\build_xab.bat
+start scripts\build_xac.bat
+start scripts\build_xad.bat
+start scripts\build_xae.bat
+start scripts\build_xaf.bat
+start scripts\build_xag.bat
+start scripts\build_xah.bat
+start scripts\build_xai.bat
+start scripts\build_xaj.bat
+start scripts\build_xak.bat
+start scripts\build_xal.bat
