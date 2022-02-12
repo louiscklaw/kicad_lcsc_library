65e4dda6f63661d1608fd49c7e615936afa7dfe1
louiscklaw <logickee@gmail.com>
2022-02-12 14:06:20 +0800

    update,

--- scripts/build_all.bat
+++ scripts/build_all.bat
@@ -9,33 +9,34 @@ start scripts\build_xac.bat
 start scripts\build_xad.bat
 start scripts\build_xae.bat
 start scripts\build_xaf.bat
-start scripts\build_xag.bat
-start scripts\build_xah.bat
-start scripts\build_xai.bat
-start scripts\build_xaj.bat
-start scripts\build_xak.bat
-start scripts\build_xal.bat
-start scripts\build_xam.bat
-start scripts\build_xan.bat
-start scripts\build_xao.bat
-start scripts\build_xap.bat
-start scripts\build_xaq.bat
-start scripts\build_xar.bat
-start scripts\build_xas.bat
-start scripts\build_xat.bat
-start scripts\build_xau.bat
-start scripts\build_xav.bat
-start scripts\build_xaw.bat
-start scripts\build_xax.bat
-start scripts\build_xay.bat
-start scripts\build_xaz.bat
-start scripts\build_xba.bat
-start scripts\build_xbb.bat
-start scripts\build_xbc.bat
-start scripts\build_xbd.bat
-start scripts\build_xbe.bat
-start scripts\build_xbf.bat
-start scripts\build_xbg.bat
-start scripts\build_xbh.bat
-start scripts\build_xbi.bat
-start scripts\build_xbj.bat
+
+@REM start scripts\build_xag.bat
+@REM start scripts\build_xah.bat
+@REM start scripts\build_xai.bat
+@REM start scripts\build_xaj.bat
+@REM start scripts\build_xak.bat
+@REM start scripts\build_xal.bat
+@REM start scripts\build_xam.bat
+@REM start scripts\build_xan.bat
+@REM start scripts\build_xao.bat
+@REM start scripts\build_xap.bat
+@REM start scripts\build_xaq.bat
+@REM start scripts\build_xar.bat
+@REM start scripts\build_xas.bat
+@REM start scripts\build_xat.bat
+@REM start scripts\build_xau.bat
+@REM start scripts\build_xav.bat
+@REM start scripts\build_xaw.bat
+@REM start scripts\build_xax.bat
+@REM start scripts\build_xay.bat
+@REM start scripts\build_xaz.bat
+@REM start scripts\build_xba.bat
+@REM start scripts\build_xbb.bat
+@REM start scripts\build_xbc.bat
+@REM start scripts\build_xbd.bat
+@REM start scripts\build_xbe.bat
+@REM start scripts\build_xbf.bat
+@REM start scripts\build_xbg.bat
+@REM start scripts\build_xbh.bat
+@REM start scripts\build_xbi.bat
+@REM start scripts\build_xbj.bat
