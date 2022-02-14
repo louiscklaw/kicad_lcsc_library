2b1d58007100cc8a6cde90764910276eaf79e5f4
louiscklaw <logickee@gmail.com>
2022-02-14 12:05:59 +0800

    update,

--- scripts/build_all.bat
+++ scripts/build_all.bat
@@ -2,7 +2,46 @@
 git fetch --all
 git pull
 
+build_amplifiers.bat
+build_analog_ics.bat
+build_audio_products_micromotors.bat
+build_battery_products.bat
+@REM build_buzzers_speakers_microphones.bat
+@REM build_capacitors.bat
+@REM build_circuit_protection.bat
+@REM build_clock_and_timing.bat
+@REM build_connectors.bat
+@REM build_crystals.bat
+@REM build_crystals_oscillators_resonators.bat
+@REM build_data_converters.bat
+@REM build_development_boards_tools.bat
+@REM build_diodes.bat
+@REM build_display_modules_led_drivers_display_drivers.bat
+@REM build_driver_ics.bat
+@REM build_electromechanical_devices_components.bat
+@REM build_embedded_peripheral_ics.bat
+@REM build_embedded_processors_controllers.bat
+@REM build_filters.bat
+@REM build_filters_emi_optimization.bat
+@REM build_functional_modules.bat
+@REM build_fuses.bat
+@REM build_hardwares_solders_accessories_batteries.bat
+@REM build_inductors_chokes_transformers.bat
+@REM build_inductors_coils_transformers.bat
+@REM build_interface_ics.bat
+@REM build_iot_communication_modules.bat
+@REM build_logic_ics.bat
+@REM build_memory.bat
+@REM build_optocouplers_leds_infrared.bat
+@REM build_optoelectronics.bat
+@REM build_others.bat
+@REM build_power_management_ics.bat
+@REM build_pushbutton_switches_relays.bat
+@REM build_relays.bat
+@REM build_resistors.bat
+@REM build_rf_radio.bat
+@REM build_sensors.bat
+@REM build_solders_accessories_batteries.bat
+@REM build_switches.bat
+@REM build_transistors.bat
 
-start scripts\build_amplifiers.bat
-
-start scripts\build_test.bat
